44d1902a99430fdb32103b24becc18a818183283
Hara Chung <harachung90@gmail.com>
2022-06-06 20:47:32 +1000

    Quiz creation update 4 - added a 'desc' to model (11)

--- Quiz/admin.py
+++ Quiz/admin.py
@@ -14,8 +14,14 @@ from .models import Quiz, Question, Answer, Mark
 class AnswerInLine(admin.TabularInline):
     model = Answer
 
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'num_of_questions', 'score_to_pass')
+    search_fields = ('name', 'num_of_questions', 'score_to_pass')
+
 
 class QuestionAdmin(SummernoteModelAdmin):
+    list_display = ('name', 'num_of_questions', 'score_to_pass')
+    search_fields = ('name', 'num_of_questions', 'score_to_pass')
     summernote_fields = ('description',)
 
 
