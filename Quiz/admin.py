71f29b0f30567bb2a7f7963d035bcdac35a2474c
Hara Chung <harachung90@gmail.com>
2022-06-06 20:51:45 +1000

    Quiz creation update 4 - added a 'desc' to model (12)

--- Quiz/admin.py
+++ Quiz/admin.py
@@ -3,29 +3,27 @@ from django.contrib import admin
 from .models import Quiz, Question, Answer, Mark
 
 
-# class AnswerInLine(admin.TabularInline):
-#     model = Answer
-#
-#
-# class QuestionAdmin(admin.ModelAdmin):
-#     inlines = [AnswerInLine]
-
-
 class AnswerInLine(admin.TabularInline):
     model = Answer
 
+
 class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerInLine]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer)
+admin.site.register(Mark)
+
+
+class QuizAdmin(admin.ModelAdmin):
     list_display = ('name', 'num_of_questions', 'score_to_pass')
     search_fields = ('name', 'num_of_questions', 'score_to_pass')
 
 
-class QuestionAdmin(SummernoteModelAdmin):
+class QuizAdmin(SummernoteModelAdmin):
     list_display = ('name', 'num_of_questions', 'score_to_pass')
     search_fields = ('name', 'num_of_questions', 'score_to_pass')
     summernote_fields = ('description',)
 
 
-admin.site.register(Quiz)
-admin.site.register(Question, QuestionAdmin)
-admin.site.register(Answer)
-admin.site.register(Mark)
+admin.site.register(Quiz, QuizAdmin)
