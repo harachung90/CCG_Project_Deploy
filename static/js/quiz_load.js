6e279154ca429afc8f6f5832d968965756e752d3
Hara Chung <harachung90@gmail.com>
2022-06-06 19:33:38 +1000

    Quiz creation update 1

--- static/js/quiz_load.js
+++ static/js/quiz_load.js
@@ -5,11 +5,13 @@ const url = window.location.href
 
 const desc1 = document.getElementById('desc1')
 const desc2 = document.getElementById('desc2')
+const desc3 = document.getElementById('desc3')
 
 function open1() {
     descBox.style.display = 'block';
     desc1.style.display = 'block';
     desc2.style.display = 'none';
+    desc3.style.display = 'none';
     startQuiz.style.display = 'block';
     const id = idButton[0].getAttribute('data-id')
 
@@ -22,6 +24,7 @@ function open2() {
     descBox.style.display = 'block';
     desc1.style.display = 'none';
     desc2.style.display = 'block';
+    desc3.style.display = 'none';
     startQuiz.style.display = 'block';
     const id = idButton[1].getAttribute('data-id')
 
@@ -30,7 +33,21 @@ function open2() {
     })
 }
 
+function open3(){
+    descBox.style.display = 'block';
+    desc1.style.display = 'none';
+    desc2.style.display = 'none';
+    desc3.style.display = 'block';
+    startQuiz.style.display = 'block';
+    const id = idButton[2].getAttribute('data-id')
+
+    startQuiz.addEventListener('click', () => {
+        window.location.href = url + id;
+    })
+}
+
 idButton[0].addEventListener('click', open1);
 idButton[1].addEventListener('click', open2);
+idButton[2].addEventListener('click', open3);
 
 
