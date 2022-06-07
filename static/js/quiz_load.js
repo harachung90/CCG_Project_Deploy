cd585a7f81d702ea1eac9d400eb81e0317a904e1
Hara Chung <harachung90@gmail.com>
2022-06-07 21:16:43 +1000

    Quiz creation update 4 - added a 'desc' to model (29)

--- static/js/quiz_load.js
+++ static/js/quiz_load.js
@@ -1,10 +1,20 @@
 $(function() {
 
+    const startQuiz = document.getElementById('start-quiz')
+    const idButton = document.getElementsByClassName('quiz_desc_button')
+    const url = window.location.href
+    const id = idButton.getAttribute('data-id')
+
+    startQuiz.addEventListener('click', () => {
+        window.location.href = url + id;
+    })
+
     $('.quiz_desc_button').click(function() {
         var $quiz = $(this).siblings().find('.quiz_desc_box');
         if ($quiz.hasClass('quiz_desc_box_active')) {
             $quiz.removeClass('quiz_desc_box_active');
             $quiz.fadeOut();
+
         } else {
             $quiz.addClass('quiz_desc_box_active');
             $quiz.fadeIn();
