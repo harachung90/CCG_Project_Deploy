3e9f3080bdf2632f972778791d0cac011c0a598f
Hara Chung <harachung90@gmail.com>
2022-06-07 20:42:05 +1000

    Quiz creation update 4 - added a 'desc' to model (28)

--- static/js/quiz_load.js
+++ static/js/quiz_load.js
@@ -4,9 +4,11 @@ $(function() {
         var $quiz = $(this).siblings().find('.quiz_desc_box');
         if ($quiz.hasClass('quiz_desc_box_active')) {
             $quiz.removeClass('quiz_desc_box_active');
+            $('.demo-container').not('div.quiz_desc_box_active').css('display', 'none');
             $quiz.fadeOut();
         } else {
             $quiz.addClass('quiz_desc_box_active');
+            $('.demo-container').not('div.quiz_desc_box_active').css('display', 'none');
             $quiz.fadeIn();
         }
     })}
