7c5a891ecd8645330403e87dd05026b0bd93f396
Hara Chung <harachung90@gmail.com>
2022-06-09 19:08:17 +1000

    Desc. New Branch, new attempt. 44 (quiz load)

--- static/js/quiz_load.js
+++ static/js/quiz_load.js
@@ -19,6 +19,10 @@ $(document).ready(function () {
             var id = $('.quiz_desc_box').eq(clickedIndex).data("id");
             console.log("quiz id: " + id);
 
+            $('.start-quiz').eq(1).on("click", function () {
+                alert("Hello!");
+            })
+
             $('.start_quiz').eq(clickedIndex).click(() => {
                 console.log("Clicked!");
                 window.location.href = url + id;
