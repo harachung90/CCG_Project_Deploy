516c16517d1a17c9c6f1d44cc38e4e4cbbbb3a80
Hara Chung <harachung90@gmail.com>
2022-08-12 14:10:25 +1000

    Debugging the search operation for MarkAdmin

--- Quiz/admin.py
+++ Quiz/admin.py
@@ -38,7 +38,7 @@ class QuizAdmin(SummernoteModelAdmin):
 
 class MarkAdmin(admin.ModelAdmin):
     list_display = ('quiz', 'user', 'score', 'user_level', 'passed')
-    search_fields = ('quiz', 'user', 'score', 'user_level', 'passed')
+    search_fields = ('score', 'user_level', 'passed')
 
 
 admin.site.register(Mark, MarkAdmin)
